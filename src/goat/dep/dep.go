f61930d4f6bae9b565e00f3e4dccf365cc832c7b
Brian Picciano <bgp2396@bellsouth.net>
2013-08-14 18:08:11 -0400

    added mercurial support

--- src/goat/dep/dep.go
+++ src/goat/dep/dep.go
@@ -72,6 +72,7 @@ var typemap = map[string]typefunc{
 	"":    goget,
 	"get": goget,
 	"git": git,
+	"hg": hg,
 }
 
 func goget(genv *GoatEnv, dep *Dependency) error {
@@ -116,6 +117,42 @@ func git(genv *GoatEnv, dep *Dependency) error {
 
 	fmt.Println("git", "clean", "-f", "-d")
 	err = exec.PipedCmd("git", "clean", "-f", "-d")
+
+	return err
+
+}
+
+func hg(genv *GoatEnv, dep *Dependency) error {
+	localloc := filepath.Join(genv.ProjRootLib, "src", dep.Path)
+
+	fmt.Println("hg","clone",dep.Location,localloc)
+	err := exec.PipedCmd("hg","clone",dep.Location,localloc)
+	if err != nil {
+		return err
+	}
+
+	origcwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	err = os.Chdir(localloc)
+	if err != nil {
+		return err
+	}
+	defer os.Chdir(origcwd)
+
+	fmt.Println("hg","pull")
+	err = exec.PipedCmd("hg","pull")
+	if err != nil {
+		return err
+	}
+
+	if dep.Reference == "" {
+		dep.Reference = "tip"
+	}
+	fmt.Println("hg", "update", "-C", dep.Reference)
+	err = exec.PipedCmd("hg", "update", "-C", dep.Reference)
 
 	return err
 
