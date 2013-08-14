9a6d925d4ff76acd138bb75c9255e47bbf5ca5e1
Brian Picciano <bgp2396@bellsouth.net>
2013-08-14 12:23:24 -0400

    fixed goat not running when not in a goat environment

--- src/goat/main/goat.go
+++ src/goat/main/goat.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"goat"
 	"goat/dep"
 	"goat/env"
 	"goat/exec"
@@ -15,23 +16,22 @@ func fatal(err error) {
 
 func printGhelp() {
 	fmt.Printf(
-		`Goat is a command-line wrapper for go which handles dependency management
-in a sane way. Check the goat docs at github.com/mediocregopher/goat for a more
+`Goat is a command-line wrapper for go which handles dependency management in a
+sane way. Check the goat docs at github.com/mediocregopher/goat for a more
 in-depth overview.
 
 Usage:
 
-    %s command [arguments]
+	%s command [arguments]
 
 The commands are:
 
-    deps    Read the Goatfile for this project and set up dependencies in the
-            lib folder. Recursively download dependencies wherever a Goatfile
-            is encountered
-    ghelp   Show this dialog
+	deps    Read the Goatfile for this project and set up dependencies in the
+	lib folder. Recursively download dependencies wherever a Goatfile is
+	encountered ghelp   Show this dialog
 
-All other commands are passed through to the go binary on your system. Try
-'%s help' for its available commands
+All other commands are passed through to the go binary on your system. Try '%s
+help' for its available commands
 `, os.Args[0], os.Args[0])
 	os.Exit(0)
 }
@@ -44,14 +44,14 @@ func main() {
 	}
 
 	projroot, err := env.FindGoatfile(cwd)
-	if err != nil {
-		fatal(err)
+	var genv *goat.GoatEnv
+	if err == nil {
+		genv = env.SetupGoatEnv(projroot)
+		if err = env.EnvPrependProj(genv); err != nil {
+			fatal(err)
+		}
 	}
 
-	genv := env.SetupGoatEnv(projroot)
-	if err = env.EnvPrependProj(genv); err != nil {
-		fatal(err)
-	}
 
 	args := os.Args[1:]
 	if len(args) < 1 {
