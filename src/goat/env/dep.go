61b34ca1b8ffd7468a0f61fbb17bcfa30bc59eb7
Brian Picciano <bgp2396@bellsouth.net>
2013-08-24 12:54:47 -0400

    made FetchDependencies' output much more readable

--- src/goat/env/dep.go
+++ src/goat/env/dep.go
@@ -3,9 +3,9 @@ package env
 import (
 	"errors"
 	. "goat/common"
-	"os"
 	"path/filepath"
 	"goat/env/deps"
+	"fmt"
 )
 
 type typefunc func(*GoatEnv, *Dependency) error
@@ -17,6 +17,22 @@ var typemap = map[string]typefunc{
 	"hg": deps.Hg,
 }
 
+func header(c string,strs ... interface{}) {
+	fmt.Printf("\n")
+	for i:=0;i<80;i++ {
+		fmt.Printf(c)
+	}
+	fmt.Printf("\n")
+
+	fmt.Println(strs...)
+
+	for i:=0;i<80;i++ {
+		fmt.Printf(c)
+	}
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+}
+
 // FetchDependencies goes and retrieves the dependencies for a given GoatEnv. If
 // the dependencies have any Goatfile's in them, this will fetch the
 // dependencies listed therein as well. All dependencies are placed in the root
@@ -25,40 +41,50 @@ var typemap = map[string]typefunc{
 func FetchDependencies(genv *GoatEnv) error {
 	var err error
 
-	if _, err = os.Stat(genv.ProjRootLib); os.IsNotExist(err) {
-		err = os.Mkdir(genv.ProjRootLib, 0755)
-		if err != nil {
-			return err
-		}
-	}
+	if len(genv.Dependencies) > 0 {
+		header("#","Downloading dependencies listed in",genv.Goatfile)
 
-	for i := range genv.Dependencies {
-		dep := &genv.Dependencies[i]
-		if dep.Path == "" {
-			dep.Path = dep.Location
-		}
+		for i := range genv.Dependencies {
+			dep := &genv.Dependencies[i]
 
-		fun, ok := typemap[dep.Type]
-		if !ok {
-			return errors.New("Unknown dependency type: " + dep.Type)
-		}
-		err = fun(genv, dep)
-		if err != nil {
-			return err
-		}
+			header("=","Retrieving dependency at:",dep.Location)
 
-		depprojroot := filepath.Join(genv.ProjRootLib, "src", dep.Path)
-		if IsProjRoot(depprojroot) {
-			depgenv,err := SetupGoatEnv(depprojroot)
+			if dep.Path == "" {
+				dep.Path = dep.Location
+			}
+
+			fun, ok := typemap[dep.Type]
+			if !ok {
+				return errors.New("Unknown dependency type: " + dep.Type)
+			}
+			err = fun(genv, dep)
 			if err != nil {
 				return err
 			}
-			ChrootEnv(depgenv, genv.ProjRoot)
-			err = FetchDependencies(depgenv)
-			if err != nil {
-				return err
+
+			depprojroot := filepath.Join(genv.ProjRootLib, "src", dep.Path)
+
+
+			if IsProjRoot(depprojroot) {
+				header("-","Reading",depprojroot,"'s dependencies")
+
+				depgenv,err := SetupGoatEnv(depprojroot)
+				if err != nil {
+					return err
+				}
+				ChrootEnv(depgenv, genv.ProjRoot)
+				err = FetchDependencies(depgenv)
+				if err != nil {
+					return err
+				}
+			} else {
+				header("-","No Goatfile found in",depprojroot)
 			}
 		}
+
+		header("#","Done downloading dependencies for",genv.Goatfile)
+	} else {
+		header("-","No dependencies listed in",genv.Goatfile)
 	}
 
 	return nil
