7371ddc1a6fb6a723714bc17adda1bf1eb01f327
Luke Jolly <lukejamesjolly@gmail.com>
2014-06-03 12:50:08 -0400

    Improved git clone and checkout logic
    asdf
    
    asdf

--- env/deps/git.go
+++ env/deps/git.go
@@ -6,15 +6,20 @@ import (
 	"github.com/mediocregopher/goat/exec"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func Git(depdir string, dep *Dependency) error {
 	localloc := filepath.Join(depdir, "src", dep.Path)
 
-	fmt.Println("git", "clone", dep.Location, localloc)
-	err := exec.PipedCmd("git", "clone", dep.Location, localloc)
-	if err != nil {
-		return err
+	if _, err := os.Stat(localloc); os.IsNotExist(err) {
+		fmt.Println("git", "clone", dep.Location, localloc)
+		err := exec.PipedCmd("git", "clone", dep.Location, localloc)
+		if err != nil {
+			return err
+		}
+	} else {
+		fmt.Println(localloc, "exists")
 	}
 
 	origcwd, err := os.Getwd()
@@ -37,6 +42,11 @@ func Git(depdir string, dep *Dependency) error {
 	if dep.Reference == "" {
 		dep.Reference = "master"
 	}
+	if exists, err := originBranchExists(dep.Reference); err != nil {
+		return err
+	} else if exists {
+		dep.Reference = "origin/" + dep.Reference
+	}
 	fmt.Println("git", "checkout", dep.Reference)
 	err = exec.PipedCmd("git", "checkout", dep.Reference)
 	if err != nil {
@@ -48,4 +58,19 @@ func Git(depdir string, dep *Dependency) error {
 
 	return err
 
+}
+
+// If 'origin/branch' exists, return true.
+func originBranchExists(branch string) (bool, error) {
+	branches, err := exec.TrimmedCmd("git", "branch", "--remote")
+	if err != nil {
+		return false, err
+	}
+	for _, b := range strings.Split(branches, "\n") {
+		b := strings.TrimSpace(b)
+		if strings.HasSuffix(b, "origin/"+branch) {
+			return true, nil
+		}
+	}
+	return false, nil
 }
